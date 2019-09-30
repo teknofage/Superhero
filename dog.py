0db24676b369b7563da05c359d509fcc5f317d55
teknofage <teknofage@gmail.com>
2019-09-29 19:29:26 -0700

    second commit

--- dog.py
+++ dog.py
@@ -1,13 +1,21 @@
+# class Dog:
+#     # Required properties are defined inside the __init__ constructor 
+#     def __init__(self, name, breed):
+#         self.name = name
+#         self.breed = breed
+#     # Methods are defined as their own named functions inside the class
+#     def bark(self):
+#         print ("Woof!")
+
+# my_dog = Dog("Rex", "SuperDog")
+# my_dog.bark()
+# print(my_dog)
+# print(my_dog.name)
+
 class Dog:
-    # Required properties are defined inside the __init__ constructor 
     def __init__(self, name, breed):
         self.name = name
         self.breed = breed
-    # Methods are defined as their own named functions inside the class
-    def bark(self):
-        print ("Woof!")
 
-my_dog = Dog("Rex", "SuperDog")
-my_dog.bark()
-print(my_dog)
-print(my_dog.name)
+    def bark(self):
+        print("Woof!")
---
+++ my-dogs.py
@@ -0,0 +1,4 @@
+import dog # we need to specify exactly what we want
+
+my_dog = dog.Dog("Rex", "SuperDog")
+my_dog.bark()
